c44e102bbd448654b9fc50c3b214b124260de323
khemraj <khemraj@rwit.io>
2025-05-16 19:10:01 +0530

    Made production schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,7 +4,10 @@ import {blockContentType} from './blockContentType'
 import {categoryType} from './categoryType'
 import {postType} from './postType'
 import {authorType} from './authorType'
+import {productType} from './productType'
+
+export const schemaTypes = [productType]
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType],
+  types: [blockContentType, categoryType, postType, authorType, productType],
 }
---
+++ sanity/schemaTypes/productType.ts
@@ -0,0 +1,47 @@
+import {defineField, defineType} from 'sanity'
+
+export const productType = defineType({
+
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 90,
+      },
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+  ],
+})
