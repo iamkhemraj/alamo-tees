ec14e7a8d47bf320de64de69a044cfb977316048
khemraj <khemraj@rwit.io>
2025-05-21 13:53:30 +0530

    Remove unwanted schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,13 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
 import {blockContentType} from './blockContentType'
-import {categoryType} from './categoryType'
-import {postType} from './postType'
-import {authorType} from './authorType'
 import {productType} from './productType'
 
 export const schemaTypes = [productType]
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType, productType],
+  types: [blockContentType, productType],
 }
