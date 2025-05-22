4f8688e6767f30f128497d6ea9f0ed802e7c439a
khemraj <khemraj@rwit.io>
2025-05-22 13:22:23 +0530

    Update the sanity strucure

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,16 +1,33 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from 'sanity/structure'
 
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title('AlamoTees Store')
     .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
+      // Global Group
+      S.listItem()
+        .title('Global')
+        .child(
+          S.list()
+            .title('Global Settings')
+            .items([
+              S.documentTypeListItem('header').title('Header'),
+              S.documentTypeListItem('footer').title('Footer'),
+            ])
+        ),
+
+      //Products 
       S.documentTypeListItem('product').title('Products'),
+
       S.divider(),
+
+      // document types
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author', 'product'].includes(item.getId()!),
+        (item) =>
+          ![
+            'header',
+            'footer',
+            'product',
+          ].includes(item.getId()!)
       ),
     ])
