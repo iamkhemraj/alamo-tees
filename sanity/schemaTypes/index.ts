c17a1f66fa08cfa0fe9457b7d196812f8f79e03a
khemraj <khemraj@rwit.io>
2025-05-22 13:22:59 +0530

    Include Header and Footer schemas

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,9 +2,11 @@ import { type SchemaTypeDefinition } from 'sanity'
 
 import {blockContentType} from './blockContentType'
 import {productType} from './productType'
+import {headerType} from './global/header'
+import {footerType} from './global/footer'
 
-export const schemaTypes = [productType]
+export const schemaTypes = [productType, headerType, footerType]
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, productType],
+  types: [blockContentType, productType, headerType , footerType],
 }
